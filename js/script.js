b0504a5801ee916c03f5b9d62a6cd669778885ca
TheGameFreak720 <dj-entityofthedark@live.com>
2018-01-01 18:09:23 -0500

    Created simons sequence with visual queues and user can click buttons with visual queues

--- js/script.js
+++ js/script.js
@@ -1,17 +1,68 @@
-const RED = 'RED';
-const BLUE = 'BLUE';
-const YELLOW = 'YELLOW';
-const GREEN = 'GREEN';
+var userSeq = [];
+var simonSeq = [];
+var id;
+var color;
+var level = 0;
 
-var simon = {
-    sendColor: function(color) {
-        console.log('The new color: ' + color);
-    }
-};
+var boardSound = [
+    'https://s3.amazonaws.com/freecodecamp/simonSound1.mp3', //green
+    'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3', //blue
+    'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3', //yellow
+    'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3' //green
+];
 
 $(document).ready(function() {
-    $('#red').click(function(){ simon.sendColor(RED); });
-    $('#blue').click(function(){ simon.sendColor(BLUE); });
-    $('#yellow').click(function(){ simon.sendColor(YELLOW); });
-    $('#green').click(function(){ simon.sendColor(GREEN); });
+
+    //Initialize board sequence
+    $('.start-btn').click(function() {
+        level++;
+        startSequence();
+    });
+
+    //User listener
+    $('.simon-btn').click(function() {
+       id = $(this).attr('id');
+       color = $(this).attr('class').split(' ')[1];
+       addClassSound(id, color)
+    });
 });
+
+//Simon Sequence
+function startSequence() {
+    console.log(level);
+    $('.display').text(level);
+    getRandomNum();
+    var i = 0;
+    var myInterval = setInterval(function() {
+
+        id = simonSeq[i];
+        color = $('#'+id).attr('class').split(' ')[1];
+        console.log(id + ' ' + color);
+        addClassSound(id, color);
+        i++;
+
+        if (i === simonSeq.length) {
+            clearInterval(myInterval);
+        }
+
+    }, 1000);
+}
+
+function getRandomNum() {
+    var random = Math.floor(Math.random() * 4);
+    simonSeq.push(random);
+}
+
+function addClassSound(id, color) {
+    $('#'+id).addClass(color + '-active');
+    //playSound(id);
+
+    setTimeout(function(){
+        $('#'+id).removeClass(color + '-active');
+    }, 500);
+}
+
+function playSound(id) {
+
+}
+
